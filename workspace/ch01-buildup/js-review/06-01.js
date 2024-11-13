ee2fb550d5f1e8b6dac4663b6ad08bc50e5376a1
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-11-13 15:54:50 +0900

    js 문법 6 동기-비동기

--- workspace/ch01-buildup/js-review/06-01.js
+++ workspace/ch01-buildup/js-review/06-01.js
@@ -1,23 +1,25 @@
-function f1(){
-  console.log(`f1 작업 시작.`);
-  console.log(`f1 작업중...`);
+function f1() {
+  console.log(`2. f1 작업 시작.`); // 2
+  console.log(`3. f1 작업중...`); // 3
   // ......
-  console.log(`f1 작업 종료.`);
-  return (`f1의 결과물`);
+  console.log(`4. f1 작업 종료.`); // 4
+  return `f1의 결과물`;
 }
 
-function f2(f1Result){
-  console.log(`${f1Result}로 f2 작업 시작.`);
-  console.log(`f2 작업중...`);
+function f2(f1Result) {
+  console.log(`5. ${f1Result}로 f2 작업 시작.`); // 5
+  console.log(`6. f2 작업중...`); // 6
   // ......
-  console.log(`f2 작업 종료.`);
-  return (`최종 결과물`);
+  console.log(`7. f2 작업 종료.`); // 7
+  return `최종 결과물`;
 }
 
-function test(){
-  
+function test() {
+  const f1Result = f1();
+  const result = f2(f1Result);
+  console.log('8.', result); // 8
 }
 
-console.log('1. 테스트 시작.');
+console.log('1. 테스트 시작.'); // 1
 test();
-console.log('테스트 완료.');
+console.log('9 .테스트 완료.'); // 9
--- workspace/ch01-buildup/js-review/06-03.js
+++ workspace/ch01-buildup/js-review/06-03.js
@@ -1,2 +1,34 @@
 // 06-02.js 복사
+function f1(callback) {
+  console.log(`2. f1 작업 시작.`); // 2
+  console.log(`3. f1 작업중...`); // 3
 
+  setTimeout(() => {
+    // ......
+    console.log(`4. f1 작업 종료.`); // 4
+    callback(`f1의 결과물`);
+  }, 1000);
+}
+
+function f2(f1Result, callback) {
+  console.log(`5. ${f1Result}로 f2 작업 시작.`); // 5
+  console.log(`6. f2 작업중...`); // 6
+
+  setTimeout(() => {
+    // ......
+    console.log(`7. f2 작업 종료.`); // 7
+    callback(`최종 결과물`);
+  }, Math.random() * 2000);
+}
+
+function test() {
+  f1((f1Result) => {
+    f2(f1Result, (result) => {
+      console.log('8.', result); // 8
+    });
+  });
+}
+
+console.log('1. 테스트 시작.'); // 1
+test();
+console.log('9 .테스트 완료.'); // 9

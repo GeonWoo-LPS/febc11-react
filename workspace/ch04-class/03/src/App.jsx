08be460b837e4f32572fc5207254f6cd1b076663
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-11-19 15:33:41 +0900

    03 클래스 컴포넌트 - 컴포넌트의 라이프 사이클

---
+++ workspace/ch04-class/03/src/App.jsx
@@ -0,0 +1,10 @@
+import {Component} from 'react';
+import ChildComponent from './LifeCycle.jsx';
+
+class App extends Component {
+  render() {
+    return <ChildComponent />;
+  }
+}
+
+export default App;

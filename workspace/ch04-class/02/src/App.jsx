53e226f6232f5dbff007c33cefb16559fb62d7bd
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-11-19 14:29:51 +0900

    02 클래스 컴포넌트 - 함수 컴포넌트와 같이 사용

---
+++ workspace/ch04-class/02/src/App.jsx
@@ -0,0 +1,10 @@
+import {Component} from 'react';
+import ChildComponent from './FunctionBase.jsx';
+
+class App extends Component {
+  render() {
+    return <ChildComponent />;
+  }
+}
+
+export default App;
---
+++ workspace/ch04-class/02/src/FunctionBase.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import {useState} from 'react';
+
+function ClickMe({level}) {
+  const [count, setCount] = useState(level || 1);
+
+  const handelClick = () => {
+    setCount(count + (level || 1));
+  };
+
+  return (
+    <div>
+      클릭횟수 X {level || 1} : {count}
+      <button onClick={handelClick}>클릭</button>
+    </div>
+  );
+}
+
+ClickMe.propTypes = {
+  level: PropTypes.number,
+};
+
+function Parent() {
+  return (
+    <div>
+      <h1>02 클래스 컴포넌트 - 함수 컴포넌트와 같이 사용</h1>
+      <ClickMe level={2} />
+      <ClickMe level={5} />
+      <ClickMe />
+    </div>
+  );
+}
+
+export default Parent;

bb857a4fb82e74752261b029abb545cdf703f5a4
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-11-19 14:09:23 +0900

    01 class 컴포넌트 사용

---
+++ workspace/ch04-class/01/src/App.jsx
@@ -0,0 +1,10 @@
+import {Component} from 'react';
+import ChildComponent from './ClassBase.jsx';
+
+class App extends Component {
+  render() {
+    return <ChildComponent />;
+  }
+}
+
+export default App;

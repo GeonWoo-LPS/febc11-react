b03e58f2b7637bd229c5a3f25cc68919b5795650
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-11-26 13:41:33 +0900

    🚧

---
+++ workspace/app/todolist/05-api/src/pages/TodoList.jsx
@@ -0,0 +1,56 @@
+import {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
+import TodoListItem from './TodoListItem';
+import useFetch from '@hooks/useFetch';
+
+// const dummyData = {
+//   items: [
+//     {_id: 1, title: '잠자기'},
+//     {_id: 2, title: '자바스크립트 복습', done: true},
+//   ],
+// };
+
+function TodoList() {
+  // const [data, setData] = useState();
+
+  // useEffect(() => {
+  //   setData(dummyData);
+  // }, []); // 마운트 된 후에 한번만 호출
+
+  // API 서버에서 목록 조회
+  const {data} = useFetch({url: '/todolist'});
+
+  // 삭제 작업
+  const handleDelete = (_id) => {
+    try {
+      // API 서버에 삭제 요청
+      alert('할일이 삭제 되었습니다.');
+
+      // 목록을 다시 조회
+    } catch (err) {
+      console.error(err);
+      alert('할일 삭제에 실패했습니다.');
+    }
+  };
+
+  const itemList = data?.items.map((item) => (
+    <TodoListItem key={item._id} item={item} handleDelete={handleDelete} />
+  ));
+
+  return (
+    <div id='main'>
+      <h2>할일 목록</h2>
+      <div className='todo'>
+        <Link to='/list/add'>추가</Link>
+        <br />
+        <form className='search'>
+          <input type='text' autoFocus />
+          <button type='submit'>검색</button>
+        </form>
+        <ul className='todolist'>{itemList}</ul>
+      </div>
+    </div>
+  );
+}
+
+export default TodoList;

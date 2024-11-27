7db62473918eba03d718dbe6e10960f988f7205e
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-11-27 16:21:48 +0900

    ✨

--- workspace/ch06-contextapi/after/src/components/Right3.jsx
+++ workspace/ch06-contextapi/after/src/components/Right3.jsx
@@ -9,9 +9,9 @@ function Right3() {
 
   const simple = useContext(SimpleContext);
 
+  // CounterContext 구독함(CounterContext의 상태변경이 리렌더링을 유발함)
   const {
     actions: {countDown, reset, countUp},
-    hello,
   } = useContext(CounterContext);
 
   return (

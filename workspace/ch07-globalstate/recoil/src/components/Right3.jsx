4290edcd4e1ddc4a63d7d1fa86450e56965e6407
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-11-28 15:31:55 +0900

    상태 관리 - recoil

--- workspace/ch07-globalstate/recoil/src/components/Right3.jsx
+++ workspace/ch07-globalstate/recoil/src/components/Right3.jsx
@@ -1,13 +1,27 @@
-import { useEffect } from 'react';
+import {counterState} from '@recoil/atoms';
+import {useEffect} from 'react';
+import {useRecoilState, useSetRecoilState} from 'recoil';
 
 function Right3() {
-  useEffect(()=>{
+  useEffect(() => {
     console.log('      # Right3 렌더링.');
   });
+
+  // getter/setter 모두 사용(구독)
+  // const [count, setCount] = useRecoilState(counterState);
+  // const countUp = (step) => {
+  //   setCount(count + step);
+  // };
+
+  const setCount = useSetRecoilState(counterState);
+  const countUp = (step) => {
+    setCount((count) => count + step);
+  };
+
   return (
     <div>
       <h3>Right3</h3>
-      <button onClick={ () => {} }>+1</button>
+      <button onClick={() => countUp(1)}>+1</button>
     </div>
   );
 }

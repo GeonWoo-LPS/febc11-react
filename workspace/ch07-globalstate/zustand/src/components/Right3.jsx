a1b9369b102aeaa2189108e2a5787894830fd01f
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-11-28 16:22:59 +0900

    상태 관리 - zustand

--- workspace/ch07-globalstate/zustand/src/components/Right3.jsx
+++ workspace/ch07-globalstate/zustand/src/components/Right3.jsx
@@ -1,13 +1,20 @@
-import { useEffect } from 'react';
+import useCounterState from '@zustand/counter';
+import {useEffect} from 'react';
 
 function Right3() {
-  useEffect(()=>{
+  useEffect(() => {
     console.log('      # Right3 렌더링.');
   });
+
+  // count를 사용하지 않아도 자동으로 구독이 되기 때문에 count 변경시 리랜더링 됨
+  // const {countUp} = useCounterState();
+
+  const countUp = useCounterState((state) => state.countUp);
+
   return (
     <div>
       <h3>Right3</h3>
-      <button onClick={ () => {} }>+1</button>
+      <button onClick={() => countUp(1)}>+1</button>
     </div>
   );
 }

148ea9d0e217239c205a82234a04d5aff28a4fd2
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-12-06 15:38:36 +0900

    ✨

--- workspace/ch11-skeleton/lion-board/src/App.jsx
+++ workspace/ch11-skeleton/lion-board/src/App.jsx
@@ -1,7 +1,8 @@
-import MainPage from '@pages/index';
+import {RouterProvider} from 'react-router-dom';
+import router from '@/routes';
 
 function App() {
-  return <MainPage />;
+  return <RouterProvider router={router} future={{v7_startTransition: true}} />;
 }
 
 export default App;

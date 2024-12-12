1f571c015a90401afdb0de1a8e605096ddc022cf
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-12-12 11:28:23 +0900

    ✨

--- workspace/ch11-skeleton/lion-board/src/pages/board/Detail.jsx
+++ workspace/ch11-skeleton/lion-board/src/pages/board/Detail.jsx
@@ -3,6 +3,7 @@ import CommentList from './CommentList';
 import useAxiosInstance from '@hooks/useAxiosInstance';
 import {useMutation, useQuery, useQueryClient} from '@tanstack/react-query';
 import useUserStore from '@zustand/userStore';
+import {Helmet} from 'react-helmet-async';
 
 export default function Detail() {
   const {user} = useUserStore();
@@ -42,49 +43,59 @@ export default function Detail() {
   }
 
   return (
-    <main className='container mx-auto mt-4 px-4'>
-      <section className='mb-8 p-4'>
-        <form onSubmit={onSubmit}>
-          <div className='font-semibold text-xl'>제목 : {data.item.title}</div>
-          <div className='text-right text-gray-400'>
-            작성자 : {data.item.user.name}
-          </div>
-          <div className='mb-4'>
-            <div>
-              <pre className='font-roboto w-full p-2 whitespace-pre-wrap'>
-                {data.item.content}
-              </pre>
-            </div>
-            <hr />
-          </div>
-          <div className='flex justify-end my-4'>
-            <Link
-              to={`/${type}`}
-              className='bg-orange-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded'
-            >
-              목록
-            </Link>
-            {user?._id === data.item.user._id && (
-              <>
-                <Link
-                  to={`/${type}/${_id}/edit`}
-                  className='bg-gray-900 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded'
-                >
-                  수정
-                </Link>
-                <button
-                  type='submit'
-                  className='bg-red-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded'
-                >
-                  삭제
-                </button>
-              </>
-            )}
-          </div>
-        </form>
-      </section>
+    <>
+      <Helmet>
+        <title>{data.item.title} - 멋사컴</title>
+        <meta property='og:title' content={data.item.title} />
+        <meta property='og:description' content={data.item.content} />
+      </Helmet>
 
-      <CommentList data={data.item.replies} />
-    </main>
+      <main className='container mx-auto mt-4 px-4'>
+        <section className='mb-8 p-4'>
+          <form onSubmit={onSubmit}>
+            <div className='font-semibold text-xl'>
+              제목 : {data.item.title}
+            </div>
+            <div className='text-right text-gray-400'>
+              작성자 : {data.item.user.name}
+            </div>
+            <div className='mb-4'>
+              <div>
+                <pre className='font-roboto w-full p-2 whitespace-pre-wrap'>
+                  {data.item.content}
+                </pre>
+              </div>
+              <hr />
+            </div>
+            <div className='flex justify-end my-4'>
+              <Link
+                to={`/${type}`}
+                className='bg-orange-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded'
+              >
+                목록
+              </Link>
+              {user?._id === data.item.user._id && (
+                <>
+                  <Link
+                    to={`/${type}/${_id}/edit`}
+                    className='bg-gray-900 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded'
+                  >
+                    수정
+                  </Link>
+                  <button
+                    type='submit'
+                    className='bg-red-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded'
+                  >
+                    삭제
+                  </button>
+                </>
+              )}
+            </div>
+          </form>
+        </section>
+
+        <CommentList data={data.item.replies} />
+      </main>
+    </>
   );
 }
